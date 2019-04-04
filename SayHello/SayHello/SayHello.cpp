aee783131f671e3720c776ab75fa640e6e91e9eb
aSpecialforce <sgzwzhm@163.com>
2019-04-04 14:52:34 +0800

    first commit

---
+++ SayHello/SayHello/SayHello.cpp
@@ -0,0 +1,16 @@
+// SayHello.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello World");
+
+	printf("\n");
+	system("pause");
+	return 0;
+}
+
